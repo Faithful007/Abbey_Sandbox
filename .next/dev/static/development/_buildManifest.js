576206766ceffc96d66b406b4685499d49e9d12d
[Abiodun Ilugbusi] <[aailugbusi@gmail.com]>
2025-11-21 16:48:51 +0900

    running

--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -1,15 +1,12 @@
 self.__BUILD_MANIFEST = {
+  "/_error": [
+    "static/chunks/pages/_error.js"
+  ],
   "/dashboard": [
     "static/chunks/pages/dashboard.js"
   ],
   "/login": [
     "static/chunks/pages/login.js"
-  ],
-  "/registration": [
-    "static/chunks/pages/registration.js"
-  ],
-  "/registration/list": [
-    "static/chunks/pages/registration/list.js"
   ],
   "__rewrites": {
     "afterFiles": [
