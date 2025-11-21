920f4024c907ce73579907b4fa5c42a3a56e1fb1
[Abiodun Ilugbusi] <[aailugbusi@gmail.com]>
2025-11-21 17:39:05 +0900

    balance

--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -1,13 +1,4 @@
 self.__BUILD_MANIFEST = {
-  "/": [
-    "static/chunks/pages/index.js"
-  ],
-  "/_error": [
-    "static/chunks/pages/_error.js"
-  ],
-  "/dashboard": [
-    "static/chunks/pages/dashboard.js"
-  ],
   "__rewrites": {
     "afterFiles": [
       {
